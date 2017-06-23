e6413a2152e62896903c373bbbbdef4df8319f74
Nils <Cyclenerd@users.noreply.github.com>
2017-06-23 15:32:30 +0200

    coolbit 31

--- files/nvidia-config.sh
+++ files/nvidia-config.sh
@@ -3,4 +3,4 @@
 # Generate an xorg.conf with faked monitors (for each of your cards)
 # You need to run this everytime you add or remove cards.
 
-sudo nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 --use-display-device="DFP-0" --connected-monitor="DFP-0"
+sudo nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=31 --use-display-device="DFP-0" --connected-monitor="DFP-0"
