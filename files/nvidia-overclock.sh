a4a168ff150905a622f32647c54a42ab90ce5378
Nils <git@velohero.com>
2017-06-23 21:51:24 +0200

    without x11vnc

--- files/nvidia-overclock.sh
+++ files/nvidia-overclock.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# Run the this script from the terminal within the X session !!!
-# Use 'x11vnc' to duplicates the first display (x0) to vnc.
-
-
+# Overclocking with nvidia-settings
 
 # GPUGraphicsClockOffset
 MY_CLOCK="150"
@@ -14,11 +11,11 @@ MY_MEM="600"
 
 for MY_DEVICE in {0..6}
 do
-	sudo nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
-	sudo nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
-	sudo nvidia-settings -a "[gpu:$MY_DEVICE]/GPUTargetFanSpeed=80"
-	sudo nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
-	sudo nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
+	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
+	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
+	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUTargetFanSpeed=80"
+	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
+	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
 done
 
 echo "Done"
