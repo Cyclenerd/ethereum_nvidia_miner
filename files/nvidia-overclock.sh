4d476ba9e2c162ab51ddc3fc2f9d494356545f5b
Nils <git@velohero.com>
2017-07-05 20:25:01 +0200

    hide error messages

--- files/nvidia-overclock.sh
+++ files/nvidia-overclock.sh
@@ -13,18 +13,22 @@ MY_MEM="600"
 # GPUTargetFanSpeed (%)
 MY_FAN="75"
 
+export DISPLAY=:0
 
 # Graphics card 1 to 6
 for MY_DEVICE in {0..5}
 do
-	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
-	# Fan speed
-	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
-	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
-	# Grafics clock
-	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
-	# Memory clock
-	DISPLAY=:0 XAUTHORITY=/var/lib/mdm/:0.Xauth nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
+	# Check if card exists
+	if nvidia-smi -i $MY_DEVICE >> /dev/null 2>&1; then
+		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
+		# Fan speed
+		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
+		nvidia-settings -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
+		# Grafics clock
+		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
+		# Memory clock
+		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
+	fi
 done
 
 echo
