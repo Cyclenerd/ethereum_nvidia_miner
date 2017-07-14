54c5e1d9541b0393b04490a0b90ddba1d0990797
metatsmith <mtsmith@mtu.edu>
2017-07-13 20:31:03 -0500

    Update nvidia-overclock.sh
    Automagically detect the number of cards rather than hard-code 6.  Fixed spelling mistake.

--- files/nvidia-overclock.sh
+++ files/nvidia-overclock.sh
@@ -15,20 +15,24 @@ fi
 
 export DISPLAY=:0
 
-# Graphics card 1 to 6
-for MY_DEVICE in {0..5}
+# Count the number of nvidia cards in /dev
+NVDEVS=`lspci | grep -i NVIDIA`
+N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
+NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
+N=`expr $N3D + $NVGA - 1`
+echo $N devices found
+
+for MY_DEVICE in `seq 0 $N` ; do
 do
-	# Check if card exists
-	if nvidia-smi -i $MY_DEVICE >> /dev/null 2>&1; then
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
-		# Fan speed
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
-		nvidia-settings -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
-		# Grafics clock
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
-		# Memory clock
-		nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
-	fi
+	# Card exists.  Set Power Mizer.
+	nvidia-settings -a "[gpu:$MY_DEVICE]/GPUPowerMizerMode=1"
+	# Fan speed
+	nvidia-settings -a "[gpu:$MY_DEVICE]/GPUFanControlState=1"
+	nvidia-settings -a "[fan:$MY_DEVICE]/GPUTargetFanSpeed=$MY_FAN"
+	# Graphics clock
+	nvidia-settings -a "[gpu:$MY_DEVICE]/GPUGraphicsClockOffset[3]=$MY_CLOCK"
+	# Memory clock
+	nvidia-settings -a "[gpu:$MY_DEVICE]/GPUMemoryTransferRateOffset[3]=$MY_MEM"
 done
 
 echo
