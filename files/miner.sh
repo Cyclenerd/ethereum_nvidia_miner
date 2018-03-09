104ecfcf8493a6035cad0952f1b38cb1b1678b23
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2018-03-09 17:24:54 +0100

    Re-added some env vars and corrected --farm-recheck value
    Some env variables may be needed for backwards compatibility
    In stratum mode --farm-recheck MUST be used with grain of salt avoiding
    to clog pool with continous submits of instant hashrate.
    Please note in ethminer 0.14+ --farm-recheck has no influence over
    hashrate display frequency as it has been locked to 5 seconds.

--- files/miner.sh
+++ files/miner.sh
@@ -20,6 +20,11 @@ sudo nvidia-smi -pm ENABLED
 sudo nvidia-smi -pl "$MY_WATT"
 echo
 
+export GPU_FORCE_64BIT_PTR=0
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
 #
@@ -29,7 +34,7 @@ export CUDA_DEVICE_ORDER=PCI_BUS_ID
 # ethminer
 # https://github.com/ethereum-mining/ethminer
 # Use -G (opencl) or -U (cuda) flag to select GPU platform.
-~/ethereum-mining/ethminer/build/ethminer/ethminer -U -S "eu1.ethermine.org:4444" -FS "us1.ethermine.org:4444" -O "$MY_ADDRESS.$MY_RIG"
+~/ethereum-mining/ethminer/build/ethminer/ethminer --farm-recheck 10000 -U -S "eu1.ethermine.org:4444" -FS "us1.ethermine.org:4444" -O "$MY_ADDRESS.$MY_RIG"
 
 # Claymore's Dual Ethereum+Decred AMD+NVIDIA GPU Miner
 # https://github.com/nanopool/Claymore-Dual-Miner
