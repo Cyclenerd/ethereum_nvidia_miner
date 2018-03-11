26d9d3e66e3e85998e6e02bbddf7e36d4c67cbed
AndreaLanfranchi <andrea.lanfranchi@gmail.com>
2018-03-11 21:24:30 +0100

    Optimized and secured update.sh
    Loop array instead of multiple statements.
    Updates downloaded into ~/updates/ directory to prevent overwriting
    without prompt (files may have been changed by user and of course
    setup.sh allows to do that).

--- files/update.sh
+++ files/update.sh
@@ -2,29 +2,22 @@
 
 #
 # update.sh
-# Author: Nils Knieling - https://github.com/Cyclenerd/ethereum_nvidia_miner
+# Author  : Nils Knieling, Andrea Lanfranchi and Contributors 
+# Project : https://github.com/Cyclenerd/ethereum_nvidia_miner
 #
 # Update files from https://github.com/Cyclenerd/ethereum_nvidia_miner
 #
 
 cd ~prospector || exit 9
+rm -fr updates
 
-echo; echo "Update '.bashrc' and '.bash_aliases'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/bashrc" -o .bashrc
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/bash_aliases" -o .bash_aliases
-echo; echo "Update '.screenrc'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/screenrc" -o .screenrc
-
-echo; echo "Update 'setup.sh'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/setup.sh" -o setup.sh
-echo; echo "Update 'miner.sh'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/miner.sh" -o miner.sh
-echo; echo "Update 'nvidia-overclock.sh'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/nvidia-overclock.sh" -o nvidia-overclock.sh
-
-echo; echo "Update 'myip.sh'"
-curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/myip.sh" -o myip.sh
-
-echo
-echo "Done"
-echo
+declare -a files
+files=(".bashrc" "bash_aliases" ".screenrc" "setup.sh" "miner.sh" "nvidia-overclock.sh" "myip.sh")
+if [ ! "${#files[@]}" == "0" ];
+then
+    for f in "${files[@]}"
+    do
+        curl -f "https://raw.githubusercontent.com/Cyclenerd/ethereum_nvidia_miner/master/files/${f}" -o "updates/${f}" --create-dirs
+    done;
+fi;
+unset files
